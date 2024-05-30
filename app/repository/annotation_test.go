61f264c6dc58fca68176aed9481a88ef59175ad0
Oleg Nikitin <5qub6pfp@duck.com>
2024-05-30 11:51:51 +0300

    feat(app): add test cases

--- app/repository/annotation_test.go
+++ app/repository/annotation_test.go
@@ -15,6 +15,40 @@ func TestAnnotation(t *testing.T) {
 	db := lib.NewDatabase(env, logger)
 	annotationRepository := NewAnnotationRepository(logger, db)
 
+	// Test cases
+	tests := map[string]struct {
+		input  string
+		result string
+	}{
+		"empty string": {
+			input:  "",
+			result: "",
+		},
+		"one character": {
+			input:  "x",
+			result: "x",
+		},
+		"long text": {
+			input:  "joke text",
+			result: "joke text",
+		},
+	}
+	for name, test := range tests {
+		test := test
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			// Store
+			annotationExp := models.Annotation{Text: test.input, JokeID: 1, From: 1, To: 1}
+			err := annotationRepository.Store(&annotationExp)
+			assert.NoError(t, err)
+
+			if got, expected := test.result, annotationExp.Text; got != expected {
+				t.Fatalf("Store(%q) returned %q; expected %q", test.input, got, expected)
+			}
+		})
+	}
+
+	// Functions test
 	// Store
 	annotationExp := models.Annotation{Text: "test1", JokeID: 1, From: 1, To: 1}
 	err := annotationRepository.Store(&annotationExp)
